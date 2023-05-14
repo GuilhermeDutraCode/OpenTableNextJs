a1fb22a2424e918963ff810b20766cff6d681ba8
Guilherme Dutra <gdutra@networxenterprise.com>
2023-05-15 09:48:32 +1000

    header and navbar created

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,11 @@ export default function Home() {
         {/* navbar */}
         <main>
             {/* Header */}
-              <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]'></div>
+              <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2'>
+                <div className='text-center mt-10'>
+                  <h1 className='text-white text-5xl font-bold mb-2'>Find your table for any occasion</h1>
+                </div>
+              </div>
             {/* Header */}
         </main>
       </main>
