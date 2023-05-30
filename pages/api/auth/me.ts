e3423cc275bd7839cd57157f414589c399e32a6e
Guilherme Dutra <gdutra@networxenterprise.com>
2023-05-30 15:39:40 +1000

    added middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,35 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import * as jose from "jose";
+
 import jwt from "jsonwebtoken";
 import prisma from "@/lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     const bearerToken = req.headers["authorization"] as string;
-
-    if(!bearerToken){
-        res.status(401).json({
-            errorMessage: "Unauthorized request",
-        });
-    }
-
-    const token = bearerToken.split(" ")[1]
-
-    if(!token){
-        res.status(401).json({
-            errorMessage: "Unauthorized request",
-        });
-    }
-
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
-
-    try {
-        await jose.jwtVerify(token, secret)
-    } catch (error) {
-        res.status(401).json({
-            errorMessage: "Unauthorized request",
-        });
-    }
-
+    const token = bearerToken.split(" ")[1];
     const payload = jwt.decode(token) as {email: string};
 
     if(!payload.email){
